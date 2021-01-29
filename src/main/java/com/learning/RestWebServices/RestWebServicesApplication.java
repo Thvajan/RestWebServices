1d5a8eb6d208a36058478c1b11a6e35b792d0343
srive <sri.venkatraghavan@gmail.com>
2021-01-29 21:26:04 +0530

    Implemented Internationalization

--- src/main/java/com/learning/RestWebServices/RestWebServicesApplication.java
+++ src/main/java/com/learning/RestWebServices/RestWebServicesApplication.java
@@ -1,7 +1,12 @@
 package com.learning.RestWebServices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestWebServicesApplication {
@@ -10,4 +15,17 @@ public class RestWebServicesApplication {
 		SpringApplication.run(RestWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.JAPAN);
+		return localeResolver;
+	}
+
+//	@Bean//method name should be messageSource; alternative can be done in application.properties
+//	public ResourceBundleMessageSource messageSource() {
+//		ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
+//		bundleMessageSource.setBasename("messages");
+//		return bundleMessageSource;
+//	}
 }
--- src/main/java/com/learning/RestWebServices/controller/UserController.java
+++ src/main/java/com/learning/RestWebServices/controller/UserController.java
@@ -2,16 +2,22 @@ package com.learning.RestWebServices.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,18 +31,24 @@ public class UserController {
 	@Autowired
 	UserDaoService userDaoService;
 
+	@Autowired
+	MessageSource messageSource;
+	
 	@GetMapping("/users")
 	public List<User> getAllUsers() {
 		return userDaoService.findAll();
 	}
 
 	@GetMapping("/users/{id}")
-	public User getUserById(@PathVariable Integer id) {
+	public EntityModel<User> getUserById(@PathVariable Integer id) {
 		User result = userDaoService.findUserById(id);
 		if (result == null) {
 			throw new UserNotFoundException("Id: " + id);
 		}
-		return result;
+		EntityModel<User> resource = EntityModel.of(result);
+		WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		return resource;
 	}
 
 	@PostMapping("/users")
@@ -53,6 +65,17 @@ public class UserController {
 		if (result == null) {
 			throw new UserNotFoundException("Id: " + id);
 		}
-
 	}
+
+	@GetMapping("/helloworldintmsg")
+	public String helloWorld(@RequestHeader(required = false, name="Accept-Language") Locale locale) {
+		return messageSource.getMessage("good.morning.message",null, locale); 
+	}
+	
+	//alt way for helloworldintmsg
+	@GetMapping("/helloworldintmsgdef")
+	public String helloWorld() {
+		return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale()); 
+	}
+	
 }
